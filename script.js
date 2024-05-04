4ce826ecef29e9d42723b5957a3124ff91555dd6
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-05-04 12:58:04 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 const makeChange = (c) => {
   // your name here
+	let values={
+		q:25,
+		d:10,
+		n:5,
+		p:1
+	}
+	let penny=0,nickel=0,dime=0,quarter=0;
+	while (c>0) {
+		if(c>values.q)
+		{
+			quarter++;
+			c-=values.q
+		}
+		else if(c>=values.d)
+		{
+			dime++
+			c-=values.d
+		}
+		else if(c>=values.n)
+		{
+			nickel++
+			c-=values.n
+		}
+		else if(c>=values.p)
+		{
+			penny++
+			c-=values.p
+		}
+	}
+	return {
+		q:quarter,
+		d:dime,
+		n:nickel,
+		p:penny
+	}
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
